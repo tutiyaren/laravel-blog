67e9f4a939b60534781a76d12ee912cf871fdb92
ren <r999b66k222@outlook.jp>
2024-05-11 20:13:37 +0900

    refactor: valueObjectをモデルに反映

--- laravel-project/app/Models/Blog_category.php
+++ laravel-project/app/Models/Blog_category.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Domain\ValueObject\BlogCategory\Id;
+use App\Domain\ValueObject\BlogCategory\BlogId;
+use App\Domain\ValueObject\BlogCategory\CategoryId;
 
 class Blog_category extends Model
 {
@@ -22,5 +25,33 @@ class Blog_category extends Model
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    // id
+    public function setIdAttribute($value)
+    {
+        $this->attributes['id'] = new Id($value);
+    }
+    public function getIdAttribute($value)
+    {
+        return $value instanceof Id ? $value->value() : $value;
+    }
+    // blogId
+    public function setBlogIdAttribute($value)
+    {
+        $this->attributes['blog_id'] = new BlogId($value);
+    }
+    public function getBlogIdAttribute($value)
+    {
+        return $value instanceof BlogId ? $value->value() : $value;
+    }
+    // categoryId
+    public function setCategoryIdAttribute($value)
+    {
+        $this->attributes['category_id'] = new CategoryId($value);
+    }
+    public function getCategoryIdAttribute($value)
+    {
+        return $value instanceof CategoryId ? $value->value() : $value;
     }
 }
